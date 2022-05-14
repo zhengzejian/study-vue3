16a1e0b5622f7bb0187c5b557f537af18a6d68e8
zhengzejian <120657866@qq.com>
2022-05-14 23:20:54 +0800

    feat: shallowReaconly

--- src/reactivity/baseHandlers.ts
+++ src/reactivity/baseHandlers.ts
@@ -1,12 +1,14 @@
 
 import { track, trigger } from './effect'
 import { ReactiveFlags, reactive, readonly } from './reactive'
-import { isObject } from '../shared'
+import { isObject, extend } from '../shared'
+
 const get = createGetter()
 const set = createSetter()
 const readonlyGet = createGetter(true)
+const shallowReadonly = createGetter(true, true)
 
-function createGetter(isReadonly = false) {
+function createGetter(isReadonly = false, shallow = false) {
     return function get(target, key) {
         if (key === ReactiveFlags.IS_REACTIVE) {
             return !isReadonly
@@ -15,6 +17,8 @@ function createGetter(isReadonly = false) {
         }
 
         const res = Reflect.get(target, key)
+
+        if (shallow) return res
 
         if (isObject(res)) {
             return isReadonly ? readonly(res) : reactive(res)
@@ -48,3 +52,7 @@ export const readonlyHandlers = {
         return true
     }
 }
+
+export let shallowReadonlyHandlers = extend({}, readonlyHandlers, {
+    get: shallowReadonly
+})
