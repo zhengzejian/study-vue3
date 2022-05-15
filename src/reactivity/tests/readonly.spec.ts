e78a94df70edafab71f0d20a5118a497982122c2
zhengzejian <120657866@qq.com>
2022-05-15 11:08:53 +0800

    feat: isProxy

--- src/reactivity/tests/readonly.spec.ts
+++ src/reactivity/tests/readonly.spec.ts
@@ -1,4 +1,4 @@
-import { readonly, isReadonly } from '../reactive'
+import { readonly, isReadonly, isProxy } from '../reactive'
 describe('readonly', () => {
     it('happy path', () => {
         const original = { foo: 1, bar: { baz: 2 } }
@@ -8,6 +8,8 @@ describe('readonly', () => {
         expect(isReadonly(original)).toBe(false)
         expect(isReadonly(wrapper.bar)).toBe(true)
         expect(isReadonly(original.bar)).toBe(false)
+        expect(isProxy(wrapper)).toBe(true)
+
         expect(wrapper.foo).toBe(1)
     })
 
